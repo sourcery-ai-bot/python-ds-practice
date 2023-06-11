9bb98815a169ee5340401ff7159989e4c4ff7854
Jerraill Murphy <80340739+JaydotMurf@users.noreply.github.com>
2023-06-11 11:21:47 +0200

    3 challenges completed

--- 03_last_element/last_element.py
+++ 03_last_element/last_element.py
@@ -1,9 +1,16 @@
 def last_element(lst):
     """Return last item in list (None if list is empty.
-    
-        >>> last_element([1, 2, 3])
-        3
-        
-        >>> last_element([]) is None
-        True
+
+    >>> last_element([1, 2, 3])
+    3
+
+    >>> last_element([]) is None
+    True
     """
+    if len(lst) is None:
+        return True
+
+    return lst[-1]
+
+
+print(last_element([10, 17, 91, 8, 13, 89]))
