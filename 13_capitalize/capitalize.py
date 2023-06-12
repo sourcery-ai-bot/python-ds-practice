ea80ac4f073e1c636eb098f614999e0bb813f517
Jerraill Murphy <Jerrraill.D.Murphy@gmail.com>
2023-06-12 16:59:41 +0200

    Finished 14 challenges

--- 13_capitalize/capitalize.py
+++ 13_capitalize/capitalize.py
@@ -1,9 +1,14 @@
 def capitalize(phrase):
     """Capitalize first letter of first word of phrase.
 
-        >>> capitalize('python')
-        'Python'
+    >>> capitalize('python')
+    'Python'
 
-        >>> capitalize('only first word')
-        'Only first word'
+    >>> capitalize('only first word')
+    'Only first word'
     """
+    capitalized_phrased = phrase.capitalize()
+    print(capitalized_phrased)
+
+
+capitalize("python")
--- 14_compact/compact.py
+++ 14_compact/compact.py
@@ -1,6 +1,11 @@
 def compact(lst):
     """Return a copy of lst with non-true elements removed.
 
-        >>> compact([0, 1, 2, '', [], False, (), None, 'All done'])
-        [1, 2, 'All done']
+    >>> compact([0, 1, 2, '', [], False, (), None, 'All done'])
+    [1, 2, 'All done']
     """
+    true_elements = list(filter(lambda ele: bool(ele), lst))
+    print(true_elements)
+
+
+compact([0, 1, 2, "", [], False, (), None, "All done"])
